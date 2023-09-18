1ee006be014c931f6dbb6745c9fb04788f818b81
LDHIUM <ldlxxlbl@gmail.com>
2023-09-18 18:30:44 +0900

    feat: 목표 값이 도달하기 전까지 무한루프를 동작

--- pig_dice.py
+++ pig_dice.py
@@ -1,4 +1,14 @@
-start = input('주사위를 던지려면 엔터를 누르세요: ')
-a = dice()
-if a == 1:
-    print('1이 나왔습니다.')
+bank = 0
+while True:
+    start = input('주사위를 던지려면 엔터를 누르세요: ')
+    a = dice()
+    if a == 1:
+        print('1이 나왔습니다.')
+        break
+    else:
+        print(f"A의 주사위는 {a} 입니다. ")
+        bank += a
+        if bank >= 50:
+            print('bank가 50이 넘었습니다. 승리하였습니다.')
+        else:
+            print(f"Bank는 {bank}입니다.")
